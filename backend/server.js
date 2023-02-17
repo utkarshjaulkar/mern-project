7f4919d6f77246331694f47cb926819d5eeacf28
Utkarsh Jaulkar <ujaulkar1604@gmail.com>
2023-02-17 11:45:14 +0530

    Some changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
+const cors = require('cors')
+const path = require('path')
 const { errorHandler } = require('./middleware/erroeMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
@@ -8,7 +10,7 @@ const port = process.env.PORT || 5000
 connectDB()
 
 const app = express()
-
+app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
