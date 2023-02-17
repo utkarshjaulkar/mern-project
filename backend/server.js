0547a19a704c1c03f89995f6faab738e81817b1b
Utkarsh Jaulkar <ujaulkar1604@gmail.com>
2023-02-17 11:37:16 +0530

    server.js changes

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
-const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/erroeMiddleware')
 const connectDB = require('./config/db')
-const cors = require('cors')
 const port = process.env.PORT || 5000
 
 connectDB()
 
 const app = express()
-app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-//Serve frontend
+// Serve frontend
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
