bdd31fa99132cbc783846177e698ddb3d3276d0d
Piyush <bhangalepiyush@gmail.com>
2023-02-20 19:00:25 +0530

    update Dashboard.jsx to remove un-necessary dependencies from useEffect

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,52 +1,53 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import GoalForm from '../components/GoalForm'
-import GoalItem from '../components/GoalItem'
-import Spinner from '../components/Spinner'
-import { getGoals, reset } from '../features/goals/goalSlice'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import GoalForm from "../components/GoalForm";
+import GoalItem from "../components/GoalItem";
+import Spinner from "../components/Spinner";
+import { getGoals, reset } from "../features/goals/goalSlice";
 
 function Dashboard() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const { user } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.auth);
   const { goals, isLoading, isError, message } = useSelector(
     (state) => state.goals
-  )
+  );
 
   useEffect(() => {
     if (isError) {
-      console.log(message)
+      console.log(message);
     }
 
     if (!user) {
-      navigate('/login')
+      navigate("/login");
     }
 
-    dispatch(getGoals())
+    dispatch(getGoals());
 
     return () => {
-      dispatch(reset())
-    }
-  }, [user, navigate, isError, message, dispatch])
+      dispatch(reset());
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, isError, message]);
 
   if (isLoading) {
-    return <Spinner />
+    return <Spinner />;
   }
 
   return (
     <>
-      <section className='heading'>
+      <section className="heading">
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
 
       <GoalForm />
 
-      <section className='content'>
+      <section className="content">
         {goals.length > 0 ? (
-          <div className='goals'>
+          <div className="goals">
             {goals.map((goal) => (
               <GoalItem key={goal._id} goal={goal} />
             ))}
@@ -56,7 +57,7 @@ function Dashboard() {
         )}
       </section>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
